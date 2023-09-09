41652599e3f4b56f94c604f5be55237c8acdc651
snoozie-v <gettingsleepyz@gmail.com>
2023-09-09 16:25:24 -0700

    update to footer

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className='footer'>
-      <h3>Non-FungibleNews 2023</h3>
+      <h3>Non-FungibleNews 2023 </h3>
     </div>
   )
 }
